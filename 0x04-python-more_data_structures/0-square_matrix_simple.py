9cf3c307d98935d88985e9a1e88cb60146f58114
Ndatimana Assa <assandatimana15@gmail.com>
2025-07-28 22:25:07 +0000

    new

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+quare_matrix_simple(matrix=[]):
+    # Create a new matrix with the same dimensions as the input matrix
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(num ** 2)
+        new_matrix.append(new_row)
+    return new_matrix
