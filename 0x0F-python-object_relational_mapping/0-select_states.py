ee6717b2ba801f78a1aa612363e98b7938d0e38e
Ndatimana Assa <assandatimana15@gmail.com>
2025-11-22 09:58:57 -0800

    new

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,34 +1,38 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
+"""
+Lists all states from the database hbtn_0e_0_usa.
+"""
+
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
-    # Arguments: username, password, database
+    # Arguments: username, password, database name
     username = sys.argv[1]
     password = sys.argv[2]
-    dbname = sys.argv[3]
+    db_name = sys.argv[3]
 
-    # Connect to MySQL
+    # Connect to MySQL server on localhost:3306
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=username,
         passwd=password,
-        db=dbname
+        db=db_name
     )
 
-    cursor = db.cursor()
+    cur = db.cursor()
 
-    # Execute query
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    # Fetch all states ordered by id ASC
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # Fetch and display results
-    rows = cursor.fetchall()
+    rows = cur.fetchall()
+
+    # Print rows exactly in the required format
     for row in rows:
         print(row)
 
-    # Close cursor and connection
-    cursor.close()
+    cur.close()
     db.close()
 
